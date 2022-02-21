417c71b384680b6b2b379502aca9da813509b606
danhnpc <visugar2000@gmail.com>
2022-02-21 16:16:48 +0700

    fix conflict

---
+++ Project3mon/app/src/main/java/com/example/project3mon/DatePickerFragment.java
@@ -0,0 +1,25 @@
+package com.example.project3mon;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle saveInstanceState){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), (EditProfileActivity) getActivity(), year, month, day);
+    }
+}
--- Project3mon/app/src/main/java/com/example/project3mon/EditProfileActivity.java
+++ Project3mon/app/src/main/java/com/example/project3mon/EditProfileActivity.java
@@ -1,18 +1,22 @@
 package com.example.project3mon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EditProfileActivity extends AppCompatActivity {
-
+public class EditProfileActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    private Button txtBirthday;
     private String selectSex;
     private Spinner spSex;
 
@@ -21,6 +25,7 @@ public class EditProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         spSex = findViewById(R.id.spSex);
+        txtBirthday = findViewById(R.id.txtBirthday);
         List<String> dataSex = new ArrayList<>();
         dataSex.add("Nam");
         dataSex.add("Nữ");
@@ -42,8 +47,17 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     public void clickToChangeBirthday(View view) {
+        DialogFragment dateFragment = new DatePickerFragment();
+        dateFragment.show(getSupportFragmentManager(), "DatePicker");
     }
 
     public void clickToSaveUserProfile(View view) {
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        int month = i1 + 1;
+        String date = i2 + "/" + month + "/" + i;
+        txtBirthday.setText(date);
     }
 }
