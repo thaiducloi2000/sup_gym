56f5db499edc70e2e7bc8e95f5083113c36ff3fc
Huy100720 <huyemz123@gmail.com>
2022-02-26 21:49:58 +0700

    sua giao dien

--- Project3mon/app/src/main/java/com/example/project3mon/Trainer1Activity.java
+++ Project3mon/app/src/main/java/com/example/project3mon/Trainer1Activity.java
@@ -3,12 +3,26 @@ package com.example.project3mon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Trainer1Activity extends AppCompatActivity {
+
+    private ImageView imgUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trainer1);
+
+        imgUser = findViewById(R.id.imageView1);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle == null){
+            return;
+        }
+
+        User user = (User) bundle.get("User");
+        imgUser.setImageResource(user.getResourceId());
+
     }
 }
