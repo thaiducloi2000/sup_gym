b0f7e415f52f84bdb2078d6ed76f4239705a1e2a
thaiducloi2000 <thaiducloi2000@gmail.com>
2022-03-03 11:30:48 +0700

    "Add Database"

---
+++ Project3mon/app/src/main/java/com/example/project3mon/DBUtils.java
@@ -0,0 +1,24 @@
+package com.example.project3mon;
+
+import android.os.StrictMode;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtils {
+
+    public Connection openConnection()
+    {
+        StrictMode.ThreadPolicy a=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(a);
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            String url=("jdbc:jtds:sqlserver://10.0.2.2:1433;databaseName=TrainerRentalManagement");
+            Connection con= DriverManager.getConnection(url,"sa","123456789");
+            return con;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- Project3mon/app/src/main/java/com/example/project3mon/MainActivity.java
+++ Project3mon/app/src/main/java/com/example/project3mon/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.project3mon;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtUserID,edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +29,39 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void clickToLogin(View view) {
-        Toast.makeText(this,"Dang Nhap Thanh Cong",Toast.LENGTH_SHORT).show();
-        Intent intent=new Intent(this,HomeMenuActivity.class);
-        startActivity(intent);
+    public void clickToLogin(View view) throws SQLException {
+        edtUserID=(EditText) findViewById(R.id.edtUserID);
+        edtPassword=(EditText) findViewById(R.id.edtPassword);
+        DBUtils c=new DBUtils();
+        Connection connection=null;
+        PreparedStatement stm=null;
+        ResultSet rs=null;
+        try {
+            connection = c.openConnection();
+            if(connection!=null){
+                String sql="Select ID FROM tblAccount WHERE userName='"+edtUserID.getText()+"' AND password='"+edtPassword.getText()+"'";
+                stm=connection.prepareStatement(sql);
+                rs=stm.executeQuery();
+                while (rs.next()){
+                    Toast.makeText(this,"Đăng Nhập Thành Công",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(this,HomeMenuActivity.class);
+                    startActivity(intent);
+                }if(!rs.next()){
+                    Toast.makeText(this,"Sai Thông Tin Đăng Nhập",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(this,"Sai Thông Tin Đăng Nhập",Toast.LENGTH_SHORT).show();
+        }finally {
+            if(rs!=null){
+                rs.close();
+            }if(stm!=null){
+                stm.close();
+            }if(connection!=null){
+                connection.close();
+            }
+        }
+
     }
-
-
-
 }
