f9c34d75fc0dc7ef446bb040873fd19b5565776e
danhnpc <visugar2000@gmail.com>
2022-03-07 22:14:00 +0700

    Add trainer profile and edit trainer profile

---
+++ Project3mon/app/src/main/java/com/example/project3mon/EditTrainerProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.project3mon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTrainerProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_trainer_profile);
+    }
+}
